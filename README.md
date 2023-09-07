892ef9af2eab583126bc9c0cf74fc5305525f8e7
drbrch <underoath2013@gmail.com>
2023-09-07 14:41:06 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mejuri_python
+This is a dedicated site for testing - https://mejuri.com/world/en/  
+Autotests for the product catalog page.  
+The user is supposed to log in, then to find the product Honey Mini Signet in the catalog of rings and add it to favorites in the shopping cart.
+
+Runnig:\
+Create venv\
+pip install -r requirements.txt\
+Run tests from root directory by\
+'pytest --maximize --browser firefox' or 'pytest --maximize --browser chrome' command\
+After pytest runnig logs are available in /logs directory.
+
+For allure:
+allure generate allure-results -c
+allure open .\allure-report\
---
+++ conftest.py
@@ -0,0 +1,106 @@
+import os
+import logging
+import datetime
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import FirefoxOptions, ChromeOptions
+
+
+class Logger:
+    def __init__(self, test_name, log_level):
+        self.logger = logging.getLogger(test_name)
+        self.logger.setLevel(log_level)
+        formatter = logging.Formatter('%(levelname)s %(message)s')
+
+        if not os.path.exists("logs"):
+            os.makedirs("logs")
+        log_file = os.path.join("logs", f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{test_name}.log")
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", help="Available browsers: firefox, chrome")
+    parser.addoption("--url", default="https://mejuri.com/world/en", help="Base URL for the tests")
+    parser.addoption("--maximize", action="store_true", help="Maximize browser window")
+    parser.addoption("--headless", action="store_true", help="Headless mode")
+    parser.addoption("--log_level", choices=["INFO", "ERROR"],
+                     default="INFO", help="Set the log level (INFO, ERROR)")
+    parser.addoption("--remote", action="store_true", help="Run tests remotely using Selenoid")
+    parser.addoption("--bv", help="Browser version")
+
+
+@pytest.fixture
+def url(request):
+    return request.config.getoption("--url")
+
+
+@pytest.fixture
+def driver(request):
+    browser_name = request.config.getoption("--browser")
+    browser_version = request.config.getoption("--bv")
+    maximize = request.config.getoption("--maximize")
+    headless = request.config.getoption("--headless")
+    log_level = request.config.getoption("--log_level")
+    remote = request.config.getoption("--remote")
+
+    logger = Logger(request.node.name, log_level)
+    logger.info("===> Test %s started at %s" %
+                (request.node.name, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
+
+    # local execution
+    if browser_name == "firefox":
+        options = FirefoxOptions()
+        if headless:
+            options.headless = True
+        driver = webdriver.Firefox(options=options)
+    elif browser_name == "chrome":
+        service = Service()
+        options = ChromeOptions()
+        if headless:
+            options.add_argument("headless=new")
+        driver = webdriver.Chrome(service=service, options=options)
+    else:
+        raise ValueError(f"Driver {browser_name} not supported")
+
+    if maximize:
+        driver.maximize_window()
+
+    if remote:
+        # remote execution with Selenoid
+        if browser_name == "firefox":
+            options = FirefoxOptions()
+            options.browser_version = browser_version
+            if headless:
+                options.headless = True
+            driver = webdriver.Remote(
+                command_executor="http://127.0.0.1:4444/wd/hub",
+                options=options
+            )
+        elif browser_name == "chrome":
+            options = ChromeOptions()
+            options.browser_version = browser_version
+            if headless:
+                options.add_argument("headless=new")
+            driver = webdriver.Remote(
+                command_executor="http://127.0.0.1:4444/wd/hub",
+                options=options
+            )
+
+    driver.logger = logger
+
+    logger.info(f"Browser {browser_name} started")
+
+    yield driver
+
+    driver.quit()
+    logger.info("===> Test %s finished at %s\n" %
+                (request.node.name, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
